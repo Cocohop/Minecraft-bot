b7784ba09e77d54e186be4d40cca5842c253b0a5
Cocohop <48159236+Cocohop@users.noreply.github.com>
2020-04-27 18:14:26 +0100

    Working, uses console however
    Gives output to output.txt

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import time, os
 import datetime
-def get_chat(value):
+def get_time(value):
     logfile = open(os.getenv("APPDATA") + "/.minecraft/logs/latest.log", "r")
-    #print(last_line[1:8])
     logfile.seek(0,2)
     while True:
         line = logfile.readline()
-        if "[CHAT] IC" in line:
+        if "[CHAT] IC" in line and "!savemessage" in line:
             last_time = line[1:9]
             hours = int(last_time[0:2])
             minutes = int(last_time[3:5])
             seconds = int(last_time[6:8])
-            last_time = datetime.time(hours, minutes , seconds)
-            print(last_time)
-
-
+            last_time = datetime.timedelta(hours = hours,minutes = minutes ,seconds = seconds)
+            first_time = last_time - value
+            return first_time
+def get_chat(time):
+    logfile = open(os.getenv("APPDATA") + "/.minecraft/logs/latest.log", "r")
+    thefile = logfile.readlines()
+    output_file = open("output.txt","w+")
+    for i in range(len(thefile)):
+        line = thefile[i]
+        if "[CHAT] IC" in line:
+            linetime = line[1:9]
+            hours = int(linetime[0:2])
+            minutes = int(linetime[3:5])
+            seconds = int(linetime[6:8])
+            linetimefinal = datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds)
+            if  linetimefinal > time:
+                output_file.write(line+"\n")
+    output_file.close()
 if __name__ == "__main__":
     value = int(input("Enter a value for the chat in minutes that you want to capture (USE INTEGERS): \n"))
     hours = value // 60
     minutes = value % 60
-    time = datetime.time(hours, minutes, 0)
-    logfile = open(os.getenv("APPDATA") + "/.minecraft/logs/latest.log", "r")
-    get_chat(time)
-
+    time = datetime.timedelta(hours = hours, minutes = minutes)
+    first_time = get_time(time)
+    get_chat(first_time)
+    #print(first_time)
 
