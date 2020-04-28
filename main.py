38583ae37fb50fb1c7bb98dc46694b652fc501c9
Cocohop <48159236+Cocohop@users.noreply.github.com>
2020-04-28 03:19:49 +0100

    Added Shout and Whisper channels

--- main.py
+++ main.py
@@ -5,28 +5,29 @@ def get_time():
     logfile.seek(0,2)
     while True:
         line = logfile.readline()
-        if "[CHAT] IC" in line and "!savemessage" in line:
-            string1 = line
-            string2 = "!savemessage"
-            count = string1.find(string2) + 13
-            value = int(string1[count:])
-            hours = value // 60
-            minutes = value % 60
-            time = datetime.timedelta(hours=hours, minutes=minutes)
-            last_time = line[1:9]
-            hours = int(last_time[0:2])
-            minutes = int(last_time[3:5])
-            seconds = int(last_time[6:8])
-            last_time = datetime.timedelta(hours = hours,minutes = minutes ,seconds = seconds)
-            first_time = last_time - time
-            return first_time
+        if "[CHAT] IC" in line or "[CHAT] W " in line or "[CHAT] S " in line:
+            if "!savemessage" in line:
+                string1 = line
+                string2 = "!savemessage"
+                count = string1.find(string2) + 13
+                value = int(string1[count:])
+                hours = value // 60
+                minutes = value % 60
+                time = datetime.timedelta(hours=hours, minutes=minutes)
+                last_time = line[1:9]
+                hours = int(last_time[0:2])
+                minutes = int(last_time[3:5])
+                seconds = int(last_time[6:8])
+                last_time = datetime.timedelta(hours = hours,minutes = minutes ,seconds = seconds)
+                first_time = last_time - time
+                return first_time
 def get_chat(time):
     logfile = open(os.getenv("APPDATA") + "/.minecraft/logs/latest.log", "r")
     thefile = logfile.readlines()
     output_file = open("output.txt","w+")
     for i in range(len(thefile)):
         line = thefile[i]
-        if "[CHAT] IC" in line:
+        if "[CHAT] IC" in line  or "[CHAT] W " in line or "[CHAT] S " in line:
             linetime = line[1:9]
             hours = int(linetime[0:2])
             minutes = int(linetime[3:5])
